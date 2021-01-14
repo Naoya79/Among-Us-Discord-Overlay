a02ed663efb9336309bbdf03817321f4740fc0a2
Naoya79 <m.naoya79@gmail.com>
2021-01-15 02:32:16 +0900

    add contents in the sidebar

--- js/content.js
+++ js/content.js
@@ -1,30 +1,62 @@
-var url = chrome.extension.getURL('images/');
+const url = chrome.extension.getURL('images/');
+const colors = ['#3f484e', '#132fd2', '#72491e', '#39fedb', 
+                '#127f2d', '#50ef3a', '#ef7d0e', '#ed53b9', 
+                '#6b30bc', '#c51111', '#d5e0ef', '#f5f558'];
 
 /*----CSS読み込み----*/
 function appendCSS(url) {
   $('body').append(`<link rel="stylesheet" href="${url}">`);
 }
+appendCSS('https://fonts.googleapis.com/icon?family=Material+Icons');
 appendCSS(chrome.extension.getURL('css/style.css'));
 
 /*----ツール要素追加----*/
-$('body').prepend(
-  `<input class="menu-label" id="menu" type="checkbox" />
+$('body').prepend(`
+  <div class="edit">
+    <a class="button" id="edit-button">Lock</a>
+    <a class="button" id="revert-button">Reset</a>
+  </div>
+`);
+$('body').append(`
+  <img id="map" src="${url}TheSkeld.png" ondragstart="return false;">
+  <input class="menu-label" id="menu" type="checkbox" />
   <label class="open menu-label" for="menu">≡</label>
   <label class="back menu-label" for="menu"></label>
   <aside>
     <label for="menu" class="close">×</label>
+    <div class="map-list">
+      <a class="button map-button" id="TheSkeld">TheSkeld</a>
+      <a class="button map-button" id="MiraHQ">MiraHQ</a>
+      <a class="button map-button" id="Polus">Polus</a>
+    </div>
     <nav>
-      Joined
-      <ul>
-        <li>list1</li>
-        <li>list2</li>
-      </ul>
-      Leaved
-      <ul>
-        <li>list3</li>
-        <li>list4</li>
-      </ul>
+      <div><i class="material-icons md-light">record_voice_over</i> Joined</div>
+      <ul id="joined-ul"></ul>
+      <div><i class="material-icons md-light transform">phone_disabled</i> Leaved</div>
+      <ul id="leaved-ul"></ul>
     </nav>
+    <table id="rule-table">
+      <tr>
+        <td>Emergency CD:</td>
+        <td><input type="number" value="15" min="0" max="60" step="5"></td>
+      </tr>
+      <tr>
+        <td>Kill Cooldown:</td>
+        <td><input type="number" value="35.0" min="10.0" max="60.0" step="2.5"></td>
+      </tr>
+      <tr>
+        <td>#Common Tasks:</td>
+        <td><input type="number" value="2" min="0" max="2" step="1"></td>
+      </tr>
+      <tr>
+        <td>#Long Tasks:</td>
+        <td><input type="number" value="3" min="0" max="3" step="1"></td>
+      </tr>
+      <tr>
+        <td>#Short Tasks:</td>
+        <td><input type="number" value="5" min="0" max="5" step="1"></td>
+      </tr>
+    </table>
     <div id="crewmate-list">
       <img class="crewmates" src="${url}playerIcons/3f484e.png" alt="#3f484e">
       <img class="crewmates" src="${url}playerIcons/132fd2.png" alt="#132fd2">
@@ -40,19 +72,7 @@ $('body').prepend(
       <img class="crewmates" src="${url}playerIcons/f5f558.png" alt="#f5f558">
     </div>
   </aside>
-  <div class="edit">
-    <a class="button" id="edit-button">Lock</a>
-    <a class="button" id="revert-button">Reset</a>
-  </div>`
-);
-$('body').append(
-  `<div class="map-list">
-    <a class="button map-button" id="TheSkeld">TheSkeld</a>
-    <a class="button map-button" id="MiraHQ">MiraHQ</a>
-    <a class="button map-button" id="Polus">Polus</a>
-  </div>
-  <img id="map" src="${url}TheSkeld.png" ondragstart="return false;">`
-);
+`);
 $('body').children('*:not(aside, .menu-label)').wrapAll('<main></main>')
 
 /*---- カラー編集切り替え ----*/
@@ -124,6 +144,61 @@ mutationObserver.observe(target, option);
 
 /*----ユーザ参加時の設定----*/
 function userJoined(node) {
+  setCrewmate(node);
+  setDraggable(node);
+  setEmergencyButton(node);
+  setColorPicker(node);
+  setDeadSwitch(node);
+}
+
+/*----ユーザ退室時の設定----*/
+function userLeaved(node) {
+  /*-- crewmate-list設定 --*/
+  const color = $(node).children('.avatar').attr('alt');
+  const src = `${url}playerIcons/${color.substr(1)}.png`;
+  if (colors.some(c => c === color)) {
+    $('#crewmate-list').append(`<img class="crewmates" src="${src}" alt="${color}">`);
+  }
+
+  /*-- sidebar設定 --*/
+  const id = $(node).attr('data-reactid');
+  const joinedUser = $(`#joined-ul .side-vs[data-reactid='${id}']`);
+  $('#leaved-ul').append(joinedUser);
+}
+
+/*----参加ユーザの初期設定----*/
+function setCrewmate(node) {
+  /*-- sidebar設定 --*/
+  const id = $(node).attr('data-reactid');
+  const leavedUser = $(`#leaved-ul .side-vs[data-reactid='${id}']`);
+  let color = 'rgba(0, 0, 0, 0)';
+
+  if (leavedUser.length != 0) {
+    const oldColor = leavedUser.attr('alt');
+    console.log(`oldColor:${oldColor}`);
+    console.log($(`#joined-ul .side-vs[alt="${oldColor}"]`));
+    if ( $(`#joined-ul .side-vs[alt="${oldColor}"]`).length == 0 ) {
+      color = oldColor;
+    }
+    leavedUser.remove();
+  }
+
+  const sideElement = $(node).clone().appendTo('#joined-ul');
+  sideElement.addClass('side-vs');
+  sideElement.attr('alt', color);
+  sideElement.css('background-color', color);
+
+  /*-- avatar設定 --*/
+  if (colors.some(c => c === color)) {
+    $(node).children('.avatar').attr('src', `${url}playerIcons/${color.substr(1)}.png`);
+    $(`#crewmate-list .crewmates[alt='${color}']`).remove();
+  }
+  $(node).children('.avatar').attr('alt', color);
+  $(node).children('.avatar').addClass('crewmates');
+}
+
+/*---- Draggable設定 ----*/
+function setDraggable(node) {
   $(node).draggable({
     scroll: false,
     distance: 20,
@@ -139,21 +214,6 @@ function userJoined(node) {
       });
     }
   });
-  $(node).children('.avatar').addClass('crewmates');
-  $(node).children('.avatar').attr('alt', '#ffffff');
-  setEmergencyButton(node);
-  //setCrewmate(node);
-  setColorPicker(node);
-  setDeadSwitch(node);
-}
-
-/*----ユーザ退室時の設定----*/
-function userLeaved(node) {
-  const color = $(node).children('.avatar').attr('alt');
-  const src = `${url}playerIcons/${color.substr(1)}.png`;
-  if (colors.some(c => c === color)) {
-    $('#crewmate-list').append(`<img class="crewmates" src="${src}" alt="${color}">`);
-  }
 }
 
 /*---- EmergencyButton設定 ----*/
@@ -172,23 +232,7 @@ function setEmergencyButton(node) {
   });
 }
 
-/*----参加ユーザにクルーメイトを設定----*/
-function setCrewmate(node) {
-  const avatar = $(node).children('.avatar');
-  const subElement = $('#crewmate-list .crewmates:first');
-  const subColor = subElement.attr('alt');
-  const subSrc = `${url}playerIcons/${subColor.substr(1)}.png`;
-
-  avatar.attr({
-    'src': subSrc,
-    'alt': subColor
-  });
-  subElement.remove();
-}
-
 /*----カラーパネルの設定----*/
-const colors = ['#3f484e', '#132fd2', '#72491e', '#39fedb', '#127f2d', '#50ef3a', '#ef7d0e', '#ed53b9', '#6b30bc', '#c51111', '#d5e0ef', '#f5f558'];
-
 function setColorPicker(node) {
   $(node).spectrum({
     showPalette: true,
